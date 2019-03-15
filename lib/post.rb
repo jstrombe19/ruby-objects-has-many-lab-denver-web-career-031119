af9876893ff8f602250bdb3417ce21360645b697
github username <github email address>
2019-03-15 08:32:50 -0600

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,28 @@
+require_relative'./author.rb'
+
+class Post
+
+  attr_accessor :title, :author
+
+  @@all = []
+
+  def initialize(title)
+    @title = title
+  #  @author = author
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if self.author == nil
+      return nil
+    else
+      return self.author.name
+    end
+  end
+
+end
